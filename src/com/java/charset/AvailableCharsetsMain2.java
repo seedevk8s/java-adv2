799f17fc9733b0a4703daa205d7d2348a3766158
seedevk8s <seedevk8s@gmail.com>
2024-12-19 21:38:12 +0900

    별칭 확인 코드 추가

--- src/com/java/charset/AvailableCharsetsMain2.java
+++ src/com/java/charset/AvailableCharsetsMain2.java
@@ -1,6 +1,7 @@
 package com.java.charset;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.SortedMap;
 
 public class AvailableCharsetsMain2 {
@@ -8,6 +9,27 @@ public class AvailableCharsetsMain2 {
         SortedMap<String, Charset> charsetSortedMap = Charset.availableCharsets();
         for (String charsetName : charsetSortedMap.keySet()) {
             System.out.println("charsetName = " + charsetName);
+        }
+
+        System.out.println(("====================================="));
+        System.out.println("charset1 = " + Charset.forName("MS949"));
+
+        System.out.println(("=================위 별칭 조회===================="));
+        Charset charset = Charset.forName("MS949");
+        for (String alias : charset.aliases()) {
+            System.out.println("alias = " + alias);
+        }
+
+        System.out.println(("=================별칭 조회2===================="));
+        Charset charset2 = Charset.forName("EUC-KR");
+        for (String alias : charset2.aliases()) {
+            System.out.println("alias = " + alias);
+        }
+
+        System.out.println(("=================별칭 조회3===================="));
+        Charset standardCharsets = StandardCharsets.UTF_8;
+        for (String alias : standardCharsets.aliases()) {
+            System.out.println("alias = " + alias);
         }
     }
 }
